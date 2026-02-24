faf98a72adc0d5351b0f8d2063385c554eb0a787
Amós Helí <amosheli2004@gmail.com>
2026-02-24 14:40:57 -0700

    Se agrego el apartado donde podemos viuslizar individualmente los elementos de las guitarras, ademas de el inicio del carrit donde ya los elementos se agregan en una lista y sus cantidades por producto, aun esta en proceso el apartado visual

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,21 @@ import { db } from "./data/db"
 function App() {
 
   const [data, setData] = useState(db)
+  const [cart, setCart] = useState([])
 
-  
+  function addToCart(item){
+
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id)
+    if(itemExists >= 0){
+      const updatedCart = [...cart]
+      updatedCart[itemExists].quantity++
+      setCart(updatedCart)
+    }else {
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+    
+  }
 
   return (
     <>  
@@ -17,8 +30,12 @@ function App() {
           <h2 className="text-center">Nuestra Colección</h2>
 
           <div className="row mt-5">
-            {data.map( ()=>(
-              <Guitar  />
+            {data.map( (guitar)=>(
+              <Guitar  
+                key={guitar.id}
+                guitar={guitar}
+                addToCart={addToCart}
+              />
             ))}
           </div>
       </main>
